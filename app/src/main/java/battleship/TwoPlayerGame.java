051183cf7072f174274a845043a6e3797c958246
jgao2 <jgao222@uw.edu>
2022-04-02 22:22:46 -0700

    specification for the Game class

--- app/src/main/java/battleship/TwoPlayerGame.java
+++ app/src/main/java/battleship/TwoPlayerGame.java
@@ -5,15 +5,25 @@ public class TwoPlayerGame extends Game {
         super(2, boardSize, shipsInfo);
     }
 
+    public TwoPlayerGame(int boardSize) {
+        super(2, boardSize);
+    }
+
     /**
      * In a two player game, it is implicit which player is the target of the attack
      * @param p the Point to attack
-     * @return a boolean representing whether or not the attack succeeded (point wasn't already guessed)
+     * @return a boolean representing whether the attack succeeded (point wasn't already guessed)
      */
     public boolean attack(Point p) {
         return false;
     }
 
+    /**
+     * In a two player game, it is implicit which player is the target of the attack
+     * @param x the x-coord of the point to attack
+     * @param y the y-coord of the point to attack
+     * @return a boolean representing whether the attack succeeded (point wasn't already guessed)
+     */
     public boolean attack(int x, int y) {
         return false;
     }
