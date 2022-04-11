ecda42a2d8da8c50b3086e1c7ed9693cd3cbb976
edward-qin <3dwardqin@gmail.com>
2022-04-10 21:26:22 -0700

    Controller interface, drawBoard added

--- app/src/main/java/textInterface/InputHandler.java
+++ app/src/main/java/textInterface/InputHandler.java
@@ -6,10 +6,19 @@ package textInterface;
 public interface InputHandler {
 
     /**
+     * Begins the application by displaying some basic information
+     * and prompting the user for the starting command.
+     */
+    void start();
+
+
+
+    /**
      * Responds to the text of the command, in some way.
      *
      * @param input The command text that was input.
      */
     void handleInput(String input);
+
 
 }
